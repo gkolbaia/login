f1ebce45a7a27c0f5c617a03d1a56a7d44b7cfe9
gkolbaia <kolbaia1234@gmail.com>
2018-10-09 19:39:47 +0400

    commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function onload() {
 
 
 
-   var newdiv = document.createElement('div');
+    var newdiv = document.createElement('div');
     document.body.appendChild(newdiv);
 
     //Email
@@ -40,7 +40,7 @@ function onload() {
     var registerButton=document.createElement('a');
 
     registerButton.textContent='Registration';
-
+    registerButton.href='#registration';
     newdiv.appendChild(registerButton);
     registerButton.style.marginRight='10px';
     registerButton.style.textDecoration='none';
@@ -55,113 +55,101 @@ function onload() {
     forgetPassword.style.textDecoration='none';
     forgetPassword.style.color='black';
 
-    //MAIN PAGE DISSAPARE
+
+
+    window.addEventListener('hashchange',function () {
+        if (location.hash=='#registration'){
+            document.body.removeChild(newdiv);
+
+
+            //REGISTRATION PAGE DIV
+            var registrationDiv=document.createElement('div');
+            document.body.appendChild(registrationDiv);
 
 
 
+            //FIRST NAME
+            var firstName=document.createElement('input');
+            firstName.type='text';
+            firstName.placeholder='First name';
+            registrationDiv.appendChild(firstName);
+            firstName.style.marginBottom='10px';
 
-    //REGISTRATION PAGE DIV
-    var registrationDiv=document.createElement('div');
-    document.body.appendChild(registrationDiv);
-    registrationDiv.style.display='none';
-
-
-    //FIRST NAME
-    var firstName=document.createElement('input');
-    firstName.type='text';
-    firstName.placeholder='First name';
-    registrationDiv.appendChild(firstName);
-    firstName.style.marginBottom='10px';
-
-    //BR
-    var BR=document.createElement('br');
-    registrationDiv.appendChild(BR);
+            //BR
+            var BR=document.createElement('br');
+            registrationDiv.appendChild(BR);
 
 
 
-    //LAST NAME
-    var lastName=document.createElement('input');
-    lastName.type='text';
-    lastName.placeholder='Last name';
-    registrationDiv.appendChild(lastName);
-    lastName.style.marginBottom='10px';
+            //LAST NAME
+            var lastName=document.createElement('input');
+            lastName.type='text';
+            lastName.placeholder='Last name';
+            registrationDiv.appendChild(lastName);
+            lastName.style.marginBottom='10px';
 
-    //BR
-    var BR=document.createElement('br');
-    registrationDiv.appendChild(BR);
+            //BR
+            var BR=document.createElement('br');
+            registrationDiv.appendChild(BR);
 
-    //MOBILE OR EMAIL
-    var mobOrEmail=document.createElement('input');
-    mobOrEmail.type='text';
-    mobOrEmail.placeholder='Mobile or email';
-    registrationDiv.appendChild(mobOrEmail);
-    mobOrEmail.style.marginBottom='10px';
+            //MOBILE OR EMAIL
+            var mobOrEmail=document.createElement('input');
+            mobOrEmail.type='text';
+            mobOrEmail.placeholder='Mobile or email';
+            registrationDiv.appendChild(mobOrEmail);
+            mobOrEmail.style.marginBottom='10px';
 
-    //BR
-    var BR=document.createElement('br');
-    registrationDiv.appendChild(BR);
+            //BR
+            var BR=document.createElement('br');
+            registrationDiv.appendChild(BR);
 
-    //PAASWORD
-    var newPassword=document.createElement('input');
-    newPassword.type='text';
-    newPassword.placeholder='Password';
-    registrationDiv.appendChild(newPassword);
-    newPassword.style.marginBottom='10px';
+            //PAASWORD
+            var newPassword=document.createElement('input');
+            newPassword.type='text';
+            newPassword.placeholder='Password';
+            registrationDiv.appendChild(newPassword);
+            newPassword.style.marginBottom='10px';
 
-    //BR
-    var BR=document.createElement('br');
-    registrationDiv.appendChild(BR);
+            //BR
+            var BR=document.createElement('br');
+            registrationDiv.appendChild(BR);
 
-    //CONFIRM PASSWORD
-    var confirmPassword=document.createElement('input');
-    confirmPassword.type='text';
-    confirmPassword.placeholder='Confirm Password';
-    registrationDiv.appendChild(confirmPassword);
-    confirmPassword.style.marginBottom='10px';
+            //CONFIRM PASSWORD
+            var confirmPassword=document.createElement('input');
+            confirmPassword.type='text';
+            confirmPassword.placeholder='Confirm Password';
+            registrationDiv.appendChild(confirmPassword);
+            confirmPassword.style.marginBottom='10px';
 
-    //BR
-    var BR=document.createElement('br');
-    registrationDiv.appendChild(BR);
+            //BR
+            var BR=document.createElement('br');
+            registrationDiv.appendChild(BR);
 
-    //SIGN UP BUTTON
-    var signUpButton=document.createElement('button');
-    signUpButton.textContent='Sign up';
-    registrationDiv.appendChild(signUpButton);
-
-
-
-    //FORGET PASSWORD
-    var forgetPassdiv=document.createElement('div');
-    document.body.appendChild(forgetPassdiv);
-    forgetPassdiv.style.display='none';
-
-    //INPUT
-    var forgetPassInput=document.createElement('input');
-    forgetPassInput.type='text';
-    forgetPassInput.placeholder='Enter email';
-    forgetPassdiv.appendChild(forgetPassInput);
-    //FORGET PASSWORD SEARCH BUTTON
-    var searchButton=document.createElement('button');
-    searchButton.textContent='Search';
-    forgetPassdiv.appendChild(searchButton);
+            //SIGN UP BUTTON
+            var signUpButton=document.createElement('button');
+            signUpButton.textContent='Sign up';
+            registrationDiv.appendChild(signUpButton);
 
 
+        }else if (location.hash=='#forgetpassword'){
+            document.body.removeChild(newdiv);
+
+            //FORGET PASSWORD
+            var forgetPassdiv=document.createElement('div');
+            document.body.appendChild(forgetPassdiv);
 
 
+            //INPUT
+            var forgetPassInput=document.createElement('input');
+            forgetPassInput.type='text';
+            forgetPassInput.placeholder='Enter email';
+            forgetPassdiv.appendChild(forgetPassInput);
+            //FORGET PASSWORD SEARCH BUTTON
+            var searchButton=document.createElement('button');
+            searchButton.textContent='Search';
+            forgetPassdiv.appendChild(searchButton);
 
-
-    //EVENT LISTENERS
-    registerButton.addEventListener('click', function () {
-        window.location.href='registration';
-        newdiv.style.display='none';
-        registrationDiv.style.display='block';
+        }
 
     })
-
-
-    forgetPassword.addEventListener('click', function () {
-        newdiv.style.display='none';
-        forgetPassdiv.style.display='block';
-    })
-
 }
